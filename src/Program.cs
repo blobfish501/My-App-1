0ad1f1fe7ef16f02f21f0f6ed5bc5364b6c566c8
blobfish501 <95372575+blobfish501@users.noreply.github.com>
2024-10-08 09:30:30 +0300

    Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -7,7 +7,6 @@
             bool task_check = true;
             while (task_check)
             {
-                Console.Clear();
                 Console.WriteLine("Выберите задачу (введите 100, чтобы прекратить выполнение): ");
                 int task = Convert.ToInt32(Console.ReadLine());
 // Задача 1
